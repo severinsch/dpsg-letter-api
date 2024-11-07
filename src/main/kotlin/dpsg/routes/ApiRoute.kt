c11292690fc1f2ee4447a1d5bea562528ac66cf2
sev <severin.schmidmeier@tum.de>
2024-11-07 13:02:22 +0900

    added health endpoint and changed docs

--- src/main/kotlin/dpsg/routes/ApiRoute.kt
+++ src/main/kotlin/dpsg/routes/ApiRoute.kt
@@ -1,10 +1,19 @@
 package dpsg.routes
 
-import io.ktor.server.routing.Routing
-import io.ktor.server.routing.route
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.health() {
+    get("/health") {
+        call.respondText("OK")
+    }
+}
 
 fun Routing.apiRoute() {
     route("/api/v1") {
         letter()
+    }
+    route("/api/v1") {
+        health()
     }
 }
